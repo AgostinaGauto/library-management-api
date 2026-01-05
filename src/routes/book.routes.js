0c683c1c2608b8c49b5473ecda493d186db9ade3
unknown <AgostinaGauto>
2026-01-05 16:03:20 -0300

    Feature: Define book CRUD routes

---
+++ src/routes/book.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const bookController = requie('../controllers/book.controller.js');
+const authMiddleware = require('../middlewares/auth.middleware');
+
+// todas las rutas de libro son privadas
+router.use(authMiddleware);
+
+// ------------- Ruta crear libro -----------------
+router.post('/', bookController.create);
+// ------------ Ruta listado ----------------
+router.get('/', bookController.getAll);
+// ------------ Ruta para obetener un libro --------------
+router.get('/:id', bookController.getById);
+// ------------ Ruta para actualizar libro --------------
+router.put('/:id', bookController.update);
+// ------------ Ruta para eliminar libro ---------------
+router.delete('/:id', bookController.remove);
+
+module.exports = router;
+
