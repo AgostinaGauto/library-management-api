3c335e148af7429125d0521660b68779025da5a9
unknown <AgostinaGauto>
2026-01-05 13:07:56 -0300

    Feature: Complete book model

---
+++ src/models/book.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+
+const Book = sequelize.define('Book', {
+
+    bookId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    title: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    editorial: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    editionDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+
+    language: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    pages: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+    state: {
+        type: DataTypes.ENUM('EN_BIBLIOTECA', 'PRESTADO', 'EN_REPARACION'),
+        allowNull: false,
+        defaultValue: 'EN_BIBLIOTECA'
+    }
+},
+
+    {
+        tableName: 'books',
+        timestamps: false
+    }
+);
+
+module.exports = Book;
